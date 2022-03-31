0b434573bc109c13fe007ddeb4902fd5f3c8ce65
Kevin Jordan <kjordan@ptc.com>
2022-03-31 14:52:24 -0400

    Initial commit

---
+++ stk/const.js
@@ -0,0 +1,10 @@
+/*
+ * This module provides a class that defines commonly used constants.
+ */
+class Const {
+
+  static IPPROTO_TCP = 6;  /* protocol number for TCP/IP */
+  static IPPROTO_UDP = 17; /* protocol number for UDP/IP */
+}
+
+module.exports = Const;
---
+++ stk/global.js
@@ -0,0 +1,9 @@
+/*
+ * This module provides a class containing static global values.
+ */
+class Global {
+
+  static programRegistry = null; /* registry of RPC programs */
+}
+
+module.exports = Global;
