58fc0cc78ad70dff2e9db0102b0566a12422f4da
Kevin Jordan <kej@kyrafre.com>
2022-07-27 23:00:45 -0400

    Make "stop" work

--- NOS2.8.7/stk.bat
+++ NOS2.8.7/stk.bat
@@ -2,5 +2,7 @@
 ::
 :: start and stop the StorageTek 4400 simulator
 ::
-if /I "%1"=="start" start "STK4400" /D ..\stk npm start
-if /I "%1"=="stop"  taskkill /FI "WINDOWTITLE eq STK4400" /F
+if /I "%1"=="start" start "STK4400" /D ..\stk cmd /c npm start
+if /I "%1"=="stop"  (
+	for /F %%p in (..\stk\pid) do taskkill /pid %%p /F /T
+)
