3ceb1c5943eb86ab8f975c636058148f9f807014
Kevin Jordan <kej@kyrafre.com>
2022-08-14 22:16:19 -0400

    Correctly mount tapes on continuation at SYSGEN(SOURCE)

--- NOS2.8.7/base-install.js
+++ NOS2.8.7/base-install.js
@@ -137,7 +137,15 @@ if (isCompletedStep("add-guest") === false) {
 
 if (isCompletedStep("sysgen-source") === false) {
   promise = promise
-  .then(() => dtc.say("Start SYSGEN(SOURCE) ..."))
+  .then(() => dtc.say("Start SYSGEN(SOURCE) ..."));
+  if (isMountTapes === false) {
+    isMountTapes = true;
+    promise = promise
+    .then(() => dtc.mount(13, 0, 1, "tapes/nos287-1.tap"))
+    .then(() => dtc.mount(13, 0, 2, "tapes/nos287-2.tap"))
+    .then(() => dtc.mount(13, 0, 3, "tapes/nos287-3.tap"));
+  }
+  promise = promise
   .then(() => dtc.dsd("[X.SYSGEN(SOURCE)"))
   .then(() => dtc.expect([ {re:/E N D   S O U R C E/} ], "printer"))
   .then(() => dtc.say("SYSGEN(SOURCE) complete"))
