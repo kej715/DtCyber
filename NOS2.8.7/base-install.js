333f774df7cd48ff16a4e24ec0a0ded6571e9a76
Kevin Jordan <kej@kyrafre.com>
2022-08-16 11:48:20 -0400

    More timing adjustments, to accommodate loaded RPi

--- NOS2.8.7/base-install.js
+++ NOS2.8.7/base-install.js
@@ -62,15 +62,15 @@ else {
   .then(() => dtc.say("Begin initial deadstart ..."))
   .then(() => dtc.dsd([
     "O!",
-    "#500#P!",
-    "#500#D=YES",
-    "#500#",
-    "#4500#NEXT.",
+    "#1000#P!",
+    "#1000#D=YES",
+    "#1000#",
+    "#5000#NEXT.",
     "#1000#]!",
     "#1000#INITIALIZE,AL,5,6,10,11,12,13.",
-    "#500#GO.",
-    "#1500#%year%%mon%%day%",
-    "#1500#%hour%%min%%sec%"
+    "#1000#GO.",
+    "#5000#%year%%mon%%day%",
+    "#3000#%hour%%min%%sec%"
   ]))
   .then(() => dtc.expect([ {re:/QUEUE FILE UTILITY COMPLETE/} ], "printer"))
   .then(() => {
