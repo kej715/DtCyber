8d0f98d2e3b30eeb3110cc87abc8e9126045f91f
Kevin Jordan <kej@kyrafre.com>
2022-09-11 21:17:18 -0400

    Increment version number

--- const.h
+++ const.h
@@ -32,9 +32,9 @@
 **  ----------------
 */
 #ifdef _DEBUG
-#define DtCyberVersion      "Desktop CYBER 5.7.9 (Debug)   "
+#define DtCyberVersion      "Desktop CYBER 5.7.10 (Debug)   "
 #else
-#define DtCyberVersion      "Desktop CYBER 5.7.9 (Release) "
+#define DtCyberVersion      "Desktop CYBER 5.7.10 (Release) "
 #endif
 
 #define DtCyberBuildDate    __DATE__ " " __TIME__
