886199486c0874e9933a8fd4b4655259c39ae11a
Kevin Jordan <kej@kyrafre.com>
2022-09-28 23:54:34 -0400

    Reference webterm directory in stop commands

--- NOS1.3/webterm-server.bat
+++ NOS1.3/webterm-server.bat
@@ -4,5 +4,5 @@
 ::
 if /I "%1"=="start" start "WebTerm-Server" /D webterm cmd /c node ..\..\webterm\webterm-server -p webterm.pid config.json
 if /I "%1"=="stop"  (
-	for /F %%p in (webterm.pid) do taskkill /pid %%p /F /T
+	for /F %%p in (webterm\webterm.pid) do taskkill /pid %%p /F /T
 )
