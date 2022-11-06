1e751fca79c7cb0b9d74b2725a28ad0d0b29530e
Kevin Jordan <kej@kyrafre.com>
2022-11-05 22:51:41 -0400

    Advance version nunber

--- const.h
+++ const.h
@@ -32,9 +32,9 @@
 **  ----------------
 */
 #ifdef _DEBUG
-#define DtCyberVersion      "Desktop CYBER 5.7.18 (Debug)   "
+#define DtCyberVersion      "Desktop CYBER 5.7.19 (Debug)   "
 #else
-#define DtCyberVersion      "Desktop CYBER 5.7.18 (Release) "
+#define DtCyberVersion      "Desktop CYBER 5.7.19 (Release) "
 #endif
 
 #define DtCyberBuildDate    __DATE__ " " __TIME__
