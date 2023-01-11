e82f26e775d389f0f75cd959b5af1b55d6ec0c60
Kevin Jordan <kej@kyrafre.com>
2023-01-11 00:09:13 -0500

    Update reference to nos287rtr.zip

--- NOS2.8.7/install.js
+++ NOS2.8.7/install.js
@@ -65,7 +65,7 @@ if (isReadyToRunInstall) {
     fs.unlinkSync("./nos287rtr.zip");
   }
   dtc.say("Download NOS 2.8.7 ready-to-run package ...")
-  .then(() => dtc.wget("https://www.dropbox.com/s/88ozv7u1qlpc27j/nos287rtr.zip?dl=1", ".", (byteCount, contentLength) => {
+  .then(() => dtc.wget("https://www.dropbox.com/s/4tey61kc0sa7swu/nos287rtr.zip?dl=1", ".", (byteCount, contentLength) => {
     maxProgressLen = utilities.reportProgress(byteCount, contentLength, maxProgressLen);
   }))
   .then(() => utilities.clearProgress(maxProgressLen))
