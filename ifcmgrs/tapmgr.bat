e4d2946d0dc08aee6094374c77790878c6fe4132
Kevin Jordan <kej@kyrafre.com>
2023-03-08 15:48:41 -0500

    Initial commit

---
+++ ifcmgrs/tapmgr.bat
@@ -0,0 +1,8 @@
+@echo off
+::
+:: Start and stop a TAP adapter. This script currently does nothing. It
+;; could be modified to start/stop TAP devices and configure their IP
+;; addresses automatically. For now, we assume that the TAP devices used
+;; by DtCyber on Windows will be created manually using standard Windows
+;; administrative tools.
+::
