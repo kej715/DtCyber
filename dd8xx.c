56de8e5e342a03fbbc8fb7ee2f1a1f208f805899
Kevin Jordan <kej@kyrefre.com>
2025-09-09 12:10:24 -0400

    Disable DEBUG

--- dd8xx.c
+++ dd8xx.c
@@ -29,7 +29,7 @@
 **--------------------------------------------------------------------------
 */
 
-#define DEBUG      1
+#define DEBUG      0
 #define DEBUG_CH 020
 #define DEBUG_EQ   0
 #define DEBUG_UN   1
